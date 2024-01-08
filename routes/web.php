b91bc4afbbe4c867166beadc4f4eabd28db84c7a
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2024-01-08 14:03:33 +0100

    add news paths for id

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return "Hello World From Laravel v10.x";
+Route::get('/meows', function () {
+    return "liste des messages";
+});
+Route::get('/meows/{id}', function (int $id) {
+    return "Message {$id}";
 });
